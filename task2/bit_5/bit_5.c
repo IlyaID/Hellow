828342e0d1572a5d1be6ef2795d712394800113a
IlyaID <ivanov.id@phystech.edu>
2023-03-06 12:08:17 +0300

    OK

--- task2/bit_5/bit_5.c
+++ task2/bit_5/bit_5.c
@@ -1,34 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(int argc, char *argv[])
+
+
+int main()
 {
-    int r = 0;
+    int result = 0;
     char str[256];
 
-    while(1 == scanf("%s", &str))
-    {
+    while(1 == scanf("%s", &str)) {
 
-        if(strcmp(str, "ma") == 0)
-            r |= 1;
-        if(strcmp(str, "cl") == 0)
-            r |= 1 << 1;
-        if(strcmp(str, "ha") == 0)
-            r |= 1 << 2;
-        if(strcmp(str, "tr") == 0)
-            r |= 1 << 3;
-        if(strcmp(str, "rd") == 0)
-            r |= 1 << 4;
-        if(strcmp(str, "bw") == 0)
-            r |= 1 << 5;
-        if(strcmp(str, "bk") == 0)
-            r |= 3 << 4;
-        if(strcmp(str, "ge") == 0)
-            r |= 1 << 6;
-        if(strcmp(str, "gy") == 0)
-            r |= 1 << 7;
-        if(strcmp(str, "da") == 0)
-            r |= 3 << 6;
+        if(strcmp(str, "ma") == 0) {
+            result |= 1;
+        }
+        if(strcmp(str, "cl") == 0) {
+            result |= 1 << 1;
+        }
+        if(strcmp(str, "ha") == 0) {
+            result |= 1 << 2;
+        }
+        if(strcmp(str, "tr") == 0) {
+            result |= 1 << 3;
+        }
+        if(strcmp(str, "rd") == 0) {
+            result |= 1 << 4;
+        }
+        if(strcmp(str, "bw") == 0) {
+            result |= 1 << 5;
+        }
+        if(strcmp(str, "bk") == 0) {
+            result |= 3 << 4;
+            }
+        if(strcmp(str, "ge") == 0) {
+            result |= 1 << 6;
+        }
+        if(strcmp(str, "gy") == 0) { 
+            result |= 1 << 7;
+        }
+        if(strcmp(str, "da") == 0) {
+            result |= 3 << 6;
+        }
     }
-    printf("%x", r);
+    printf("%x", result);
     return 0;
+}
