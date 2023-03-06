4acbceb26ea8eb069c23badf7b8869f351aeddb5
IlyaID <ivanov.id@phystech.edu>
2023-03-06 17:37:01 +0300

    Good

--- task3/cach/cavhe.c
+++ task3/cach/cavhe.c
@@ -15,11 +15,29 @@ int Evklid(int x, int y){
 }
 
 int main(){
-    int n = 0;
-    scanf("%d", &n);
+    int A[1024][1024];
+    int B[1024][1024];
+    int C[1024][1024];
+    int n = 0, d =0;
+    scanf("%d %d", &n, &d);
     for (unsigned int i = 0; i < n; i++) {
         for (unsigned int j = 0; j < n; j++) {
-            
+           A[i][j] = Evklid(i + 1, j +1 );
+           B[i][j] = Evklid(n - i + 1, n -j +1 );
         }
     }
+    
+    int coun = 0;
+    int sum = 0;
+    for (unsigned int i = 0; i < n; i++) {
+        for (unsigned int j = 0; j < n; j++) {
+           for(unsigned k = 0; k < n; ) {
+                sum += A[i][k] * B[k][j];
+                if (sum % d ==0) coun++;
+           }
+        }
+    }
+
+return 0;
+
 }
