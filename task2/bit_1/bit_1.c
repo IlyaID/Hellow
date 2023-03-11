486566ca1ba7e24ec50bbd4010ac5c151a81f6a8
IlyaID <ivanov.id@phystech.edu>
2023-03-11 15:10:48 +0300

    U

--- task2/bit_1/bit_1.c
+++ task2/bit_1/bit_1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-    int x;
-    sscanf(argv[1], "%x", &x);
-    if(x & 1)
+int main(int argc, char *argv[]) {
+    int input;
+    sscanf(argv[1], "%x", &input);
+    if(input & 1) {
         printf("ma");
-    else 
+    }
+    else{ 
         printf("fe");
-
+    }
     return 0;
 }
