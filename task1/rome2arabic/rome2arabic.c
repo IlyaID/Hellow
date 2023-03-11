c85ee4e9d8f8219f4a81fe4880ac6d4fee8e2809
IlyaID <ivanov.id@phystech.edu>
2023-03-11 17:29:34 +0300

    U

--- task1/rome2arabic/rome2arabic.c
+++ task1/rome2arabic/rome2arabic.c
@@ -1,24 +1,39 @@
 #include <string.h>
 #include <stdio.h>
  
-void arabic_to_Rome(int n, char* result) {
+void arabic_to_Rome(int number, char* result) {
  
-    const char* str_rome[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-    int values_rome[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    const char* str_rome[13] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+    int values_rome[13] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
  
-    result[0] = 0;
+    
     for (int i = 0; i < 13; ++i) {
-        while (n - values_rome[i] >= 0) {
+        while (number - values_rome[i] >= 0) {
             strcat(result, str_rome[i]);
-            n -= values_rome[i];
+            number -= values_rome[i];
         }
     }
 }
+
+int Rome_to_arabic(char* number) {
+ 
+    const char* str_rome[13] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+    int values_rome[13] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+ 
+    int result = 0;
+    for (int i = 0; i < 64; i++) {
+        for (int j = 0; j < 13; j++){
+            if (strchr(str_rome[j], number[i]) == 0) {
+                result += values_rome[j];
+            }
+        }
+    }
+    return result;
+}
  
 int main() {
-    char str[64];
-    int num;
-    scanf("%d", &num);
-    arabic_to_Rome(num, str);
-    printf("%d = %s\n", num, str);
+    char* str[64];
+    scanf("%s", &str);
+    printf("%d\n",Rome_to_arabic(*str));
+    return 0;
 }
