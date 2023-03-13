036fe7858125aba3cece09cc56fe3e8e96dfcabd
IlyaID <ivanov.id@phystech.edu>
2023-03-13 17:23:59 +0300

    Updete

--- task3/cach/cavhe.c
+++ task3/cach/cavhe.c
@@ -4,40 +4,45 @@
 
 int Evklid(int x, int y){
     if (x != y) {
-        if (x > y){
+        if (x > y) {
             Evklid(x -y, y);
         }
-        else{
+        else {
             Evklid(x, y - x); 
         }
      }
-     return x;
+     else {
+        return x;
+     }
+     
 }
 
 int main(){
-    int A[20][20];
-    int B[20][20];
-    unsigned int n = 0, d =0;
+    int A[1024][1024];
+    int B[1024][1024];
+     int n = 0, d =0;
     scanf("%d %d", &n, &d);
-    for (unsigned int i = 0; i < n; i++) {
-        for (unsigned int j = 0; j < n; j++) {
+    for ( int i = 0; i < n; i++) {
+        for ( int j = 0; j < n; j++) {
            A[i][j] = Evklid(i + 1, j + 1);
            B[i][j] = Evklid(n - i + 1, n - j + 1);
         }
     }
     
-    unsigned int coun = 0;
-    int sum = 0;
-    for (unsigned int i = 0; i < n; i++) {
-        for (unsigned int j = 0; j < n; j++) {
-           for(unsigned k = 0; k < n; k++ ) {
-                sum += A[i][k] * B[k][j];
-                if (sum % d == 0) coun++;
-           }
+     int coun = 0;
+    
+    for ( int i = 0; i < n; i++) {
+        for ( int j = 0; j < n; j++) {
+            int sum = 0;
+            for( int k = 0; k < n; k++ ) {
+                sum += A[i][k] * B[j][k];
+            }
+            if (sum % d == 0) coun++;
+           
         }
     }
 
-    printf("%u", coun);
+    printf("%d", coun);
 
 return 0;
 
