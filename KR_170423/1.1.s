c5f0ed374a86113fb65aeaf57f53e9624a6dac13
IlyaID <ivanov.id@phystech.edu>
2023-04-17 17:32:09 +0300

    1.1

--- KR_170423/1.1.s
+++ KR_170423/1.1.s
@@ -1,3 +1,19 @@
-;У = (x / У) * (x % y)
-;b = (a - 500000) % 10;
+;Task a) У = (x / У) * (x % y)
+
+;y = rbx = 5
+;x = rax = 10
+
+mov rax, 10
+mov rbx, 5
+idiv rbx
+add rax, rdx
+
+;rax = У = (x / У) * (x % y)
+
+
+;Task b) b = (a - 500000) % 10;
+
+;
+
+
 
