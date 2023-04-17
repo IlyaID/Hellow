eae19c7c1b525d39b3b6335d6e11ff0b638b3091
IlyaID <ivanov.id@phystech.edu>
2023-04-17 16:03:13 +0300

    Not solved!

---
+++ asm2/O.s
@@ -0,0 +1,23 @@
+section .bss
+    buf resb 1
+
+section .text
+    global _start
+
+    _start: 
+        mov eax, 3
+        mov ebx, 0
+        mov ecx, buf
+        mov edx, 1
+        int 0x80
+
+        mov al, [buf]
+        or al, 7
+        mov [buf], al
+
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, buf
+        mov edx, 1
+        int 0x80
+
