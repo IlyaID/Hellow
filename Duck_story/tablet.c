1fa243acbe4fcf1f6e26fc2df972233b9defc302
IlyaID <ivanov.id@phystech.edu>
2023-05-15 16:22:34 +0300

    UPd

---
+++ Duck_story/tablet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+int main() {
+    FILE* fp = fopen("source", "rb");
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    uint32_t* buff = (uint32_t*)calloc(size, sizeof(char));
+    fread(buff, sizeof(char), size, fp);
+    fclose(fp);
+    int i = 0;
+    while(1) {
+        if(buff[i] == (uint32_t)0x54fffee1)
+        {
+            buff[i] = (uint32_t)0xd503201f;
+            break;
+        }
+        i++;
+    }
+    fp = fopen("cracked", "wb");
+    fwrite(buff, sizeof(char), size, fp);
+    fclose(fp);
+    return 0;
+}
